c9f6871bc54ebb590b20e32e4d87de151c5bf10a
uo277303 <uo277303@EII-L05-12.aulasuo.uniovi.es>
2022-03-11 16:49:29 +0100

    Trominó terminado

--- src/p3/tromino/Tromino.java
+++ src/p3/tromino/Tromino.java
@@ -2,22 +2,14 @@ package p3.tromino;
 
 public class Tromino {
 
-	static int[][] tablero;
+	int[][] tablero;
 	int count = 0;
 
-	/**
-	 * Método que crea el tablero inicial y ejecuta el método
-	 * 
-	 * @param n      Tamaño del tablero (nxn)
-	 * @param xVacio Posición x de la casilla vacía
-	 * @param yVacio Posición y de la casilla vacía
-	 * @throws IllegalArgumentException Si algún parámetro es inválido
-	 */
 	public void crearTablero(int n, int xVacio, int yVacio) {
 		if (n > 0 && potenciaDe2(n) && xVacio < n && yVacio < n) {
 			tablero = new int[n][n];
 			tablero[xVacio][yVacio] = -1;
-			tromino(tablero, n, xVacio, yVacio, 0, 0);
+			tromino(xVacio, yVacio, n, 0, 0);
 		} else {
 			throw new IllegalArgumentException("Error en los parámetros.");
 		}
@@ -27,70 +19,95 @@ public class Tromino {
 		return Math.log(x) / Math.log(2) % 1 == 0;
 	}
 
-	/**
-	 * Método recursivo que completa los subtableros añadiendo las fichas en los
-	 * lugares correspondientes
-	 * 
-	 * @param st  Tablero
-	 * @param n   Tamaño del subtablero
-	 * @param xOc Posición x de la casilla ocupada
-	 * @param yOc Posición y de la casilla ocupada
-	 * @param xEm Posición x del tablero anterior en la que empieza el subtablero
-	 * @param yEm Posición y del tablero anterior en la que empieza el subtablero
-	 */
-	public void tromino(int[][] st, int n, int xOc, int yOc, int xEm, int yEm) {
-		// Guardo el centro del tablero (esquina inferior derecha del primer cuadrante)
-		int[] centro = { xEm + n / 2 - 1, yEm + n / 2 - 1 };
+	public void tromino(int x, int y, int n, int orig_x, int orig_y) {
+		int cuadrante = buscarCuadrante(orig_x + n / 2 - 1, orig_y + n / 2 - 1, x, y);
 
-		int cuadrante = buscarCuadrante(centro[0], centro[1], xOc, yOc);
+		// Centro del subtablero
+		int[] c = { orig_x + n / 2 - 1, orig_y + n / 2 - 1 };
 
-		if (cuadrante == 1) { // Primer cuadrante
-			count++;
-			st[centro[0]][centro[1] + 1] = count;
-			st[centro[0] + 1][centro[1]] = count;
-			st[centro[0] + 1][centro[1] + 1] = count;
-			imprimirTablero();
-		} else if (cuadrante == 2) { // Segundo cuadrante
-			count++;
-			st[centro[0]][centro[1]] = count;
-			st[centro[0] + 1][centro[1]] = count;
-			st[centro[0] + 1][centro[1] + 1] = count;
-			imprimirTablero();
-		} else if (cuadrante == 3) { // Tercer cuadrante
-			count++;
-			st[centro[0]][centro[1]] = count;
-			st[centro[0]][centro[1] + 1] = count;
-			st[centro[0] + 1][centro[1] + 1] = count;
-			imprimirTablero();
-		} else { // Cuarto cuadrante
-			count++;
-			st[centro[0]][centro[1]] = count;
-			st[centro[0] + 1][centro[1]] = count;
-			st[centro[0]][centro[1] + 1] = count;
-			imprimirTablero();
-		}
+		count++;
 
 		if (n > 2) {
 			if (cuadrante == 1) {
-				tromino(st, n / 2, centro[0], centro[1], xOc, yOc);
-				tromino(st, n / 2, centro[0] + 1, centro[1], xEm + n / 2, yEm);
-				tromino(st, n / 2, centro[0], centro[1] + 1, xEm, yEm + n / 2);
-				tromino(st, n / 2, centro[0] + 1, centro[1] + 1, xEm + n / 2, yEm + n / 2);
+				tablero[c[0]][c[1] + 1] = count;
+				tablero[c[0] + 1][c[1]] = count;
+				tablero[c[0] + 1][c[1] + 1] = count;
+
+				imprimirTablero();
+
+				tromino(x, y, n / 2, orig_x, orig_y);
+				tromino(c[0], c[1] + 1, n / 2, orig_x, orig_y + n / 2);
+				tromino(c[0] + 1, c[1], n / 2, orig_x + n / 2, orig_y);
+				tromino(c[0] + 1, c[1] + 1, n / 2, orig_x + n / 2, orig_y + n / 2);
+
 			} else if (cuadrante == 2) {
-				tromino(st, n / 2, centro[0], centro[1], xEm, yEm);
-				tromino(st, n / 2, centro[0] + 1, centro[1], xOc, yOc);
-				tromino(st, n / 2, centro[0], centro[1] + 1, xEm, yEm + n / 2);
-				tromino(st, n / 2, centro[0] + 1, centro[1] + 1, xEm + n / 2, yEm + n / 2);
+				tablero[c[0]][c[1]] = count;
+				tablero[c[0] + 1][c[1]] = count;
+				tablero[c[0] + 1][c[1] + 1] = count;
+
+				imprimirTablero();
+
+				tromino(c[0], c[1], n / 2, orig_x, orig_y);
+				tromino(c[0] + 1, c[1], n / 2, orig_x, orig_y + n / 2);
+				tromino(x, y, n / 2, orig_x + n / 2, orig_y);
+				tromino(c[0] + 1, c[1] + 1, n / 2, orig_x + n / 2, orig_y + n / 2);
+
 			} else if (cuadrante == 3) {
-				tromino(st, n / 2, centro[0], centro[1], xEm, yEm);
-				tromino(st, n / 2, centro[0] + 1, centro[1], xEm + n / 2, yEm);
-				tromino(st, n / 2, centro[0], centro[1] + 1, xOc, yOc);
-				tromino(st, n / 2, centro[0] + 1, centro[1] + 1, xEm + n / 2, yEm + n / 2);
+				tablero[c[0]][c[1]] = count;
+				tablero[c[0]][c[1] + 1] = count;
+				tablero[c[0] + 1][c[1] + 1] = count;
+
+				imprimirTablero();
+
+				tromino(c[0], c[1], n / 2, orig_x, orig_y);
+				tromino(x, y, n / 2, orig_x, orig_y + n / 2);
+				tromino(c[0], c[1] + 1, n / 2, orig_x + n / 2, orig_y);
+				tromino(c[0] + 1, c[1] + 1, n / 2, orig_x + n / 2, orig_y + n / 2);
+
 			} else {
-				tromino(st, n / 2, centro[0], centro[1], xEm, yEm);
-				tromino(st, n / 2, centro[0] + 1, centro[1], xEm + n / 2, yEm);
-				tromino(st, n / 2, centro[0], centro[1] + 1, xEm, yEm + n / 2);
-				tromino(st, n / 2, centro[0] + 1, centro[1] + 1, xOc, yOc);
+				tablero[c[0]][c[1]] = count;
+				tablero[c[0] + 1][c[1]] = count;
+				tablero[c[0]][c[1] + 1] = count;
+
+				imprimirTablero();
+
+				tromino(c[0], c[1], n / 2, orig_x, orig_y);
+				tromino(c[0], c[1] + 1, n / 2, orig_x, orig_y + n / 2);
+				tromino(c[0] + 1, c[1], n / 2, orig_x + n / 2, orig_y);
+				tromino(x, y, n / 2, orig_x + n / 2, orig_y + n / 2);
+			}
+		} else { // Caso básico
+			if (tablero[orig_x][orig_y] != 0) {
+
+				tablero[orig_x + 1][orig_y] = count;
+				tablero[orig_x][orig_y + 1] = count;
+				tablero[orig_x + 1][orig_y + 1] = count;
+
+				imprimirTablero();
+
+			} else if (tablero[orig_x + 1][orig_y] != 0) {
+
+				tablero[orig_x][orig_y] = count;
+				tablero[orig_x][orig_y + 1] = count;
+				tablero[orig_x + 1][orig_y + 1] = count;
+
+				imprimirTablero();
+
+			} else if (tablero[orig_x][orig_y + 1] != 0) {
+
+				tablero[orig_x][orig_y] = count;
+				tablero[orig_x + 1][orig_y] = count;
+				tablero[orig_x + 1][orig_y + 1] = count;
+
+				imprimirTablero();
+
+			} else {
+
+				tablero[orig_x][orig_y] = count;
+				tablero[orig_x + 1][orig_y] = count;
+				tablero[orig_x][orig_y + 1] = count;
+
+				imprimirTablero();
 			}
 		}
 	}
@@ -110,90 +127,16 @@ public class Tromino {
 		System.out.println("--- TROMINÓ ---");
 		for (int i = 0; i < tablero[0].length; i++) {
 			for (int j = 0; j < tablero[0].length; j++) {
-				System.out.print("\t|\t" + tablero[i][j]);
+				if (j != 0) {
+					System.out.print("\t");
+				}
+				System.out.print("|\t" + tablero[i][j]);
 				if (j == tablero[0].length - 1) {
 					System.out.print("\t|\n");
 				}
 			}
 		}
 		System.out.println();
-	}
-
-	///////////////////////
-
-	public void tromino_2(int[][] st, int n, int xOc, int yOc, int xEm, int yEm) {
-		// Guardo el centro del tablero (esquina inferior derecha del primer cuadrante)
-		int[] centro = { xEm + n / 2 - 1, yEm + n / 2 - 1 };
-
-		if (n <= 2) {
-			count++;
-			int aux = st[xOc][yOc];
-			st[centro[0]][centro[1]] = count;
-			st[centro[0] + 1][centro[1]] = count;
-			st[centro[0]][centro[1] + 1] = count;
-			st[centro[0] + 1][centro[1] + 1] = count;
-			st[xOc][yOc] = aux;
-		} else {
-			// Divido el tablero en 4 cuadrantes, guardando para cada uno la posición de
-			// arriba a la izquierda
-			int[] C1 = { xEm, yEm };
-			int[] C2 = { xEm, yEm + n / 2 };
-			int[] C3 = { xEm + n / 2, yEm };
-			int[] C4 = { xEm + n / 2, yEm + n / 2 };
-
-			// Casilla que se marca según el cuadrante
-			int[] mC1 = { centro[0], centro[1] };
-			int[] mC2 = { centro[0], centro[1] + 1 };
-			int[] mC3 = { centro[0] + 1, centro[1] };
-			int[] mC4 = { centro[0] + 1, centro[1] + 1 };
-
-			if (xOc <= centro[0] && yOc <= centro[1]) { // Primer cuadrante
-				count++;
-				st[centro[0]][centro[1] + 1] = count;
-				st[centro[0] + 1][centro[1]] = count;
-				st[centro[0] + 1][centro[1] + 1] = count;
-				mC1[0] = xOc;
-				mC1[1] = yOc;
-			} else if (xOc <= centro[0] && yOc > centro[1]) { // Segundo cuadrante
-				count++;
-				st[centro[0]][centro[1]] = count;
-				st[centro[0] + 1][centro[1]] = count;
-				st[centro[0] + 1][centro[1] + 1] = count;
-				mC2[0] = xOc;
-				mC2[1] = yOc;
-			} else if (xOc > centro[0] && yOc <= centro[1]) { // Tercer cuadrante
-				count++;
-				st[centro[0]][centro[1]] = count;
-				st[centro[0]][centro[1] + 1] = count;
-				st[centro[0] + 1][centro[1] + 1] = count;
-				mC3[0] = xOc;
-				mC3[1] = yOc;
-			} else { // Cuarto cuadrante
-				count++;
-				st[centro[0]][centro[1]] = count;
-				st[centro[0]][centro[1] + 1] = count;
-				st[centro[0] + 1][centro[1]] = count;
-				mC4[0] = xOc;
-				mC4[1] = yOc;
-			}
-
-			imprimirTablero();
-
-			tromino(st, n / 2, mC1[0], mC1[1], C1[0], C1[1]);
-			tromino(st, n / 2, mC2[0], mC2[1], C2[0], C2[1]);
-			tromino(st, n / 2, mC3[0], mC3[1], C3[0], C3[1]);
-			tromino(st, n / 2, mC4[0], mC4[1], C4[0], C4[1]);
-		}
-	}
-
-	public void crearTablero_2(int n, int xVacio, int yVacio) {
-		if (n > 0 && potenciaDe2(n) && xVacio < n && yVacio < n) {
-			tablero = new int[n][n];
-			tablero[xVacio][yVacio] = -1;
-			tromino_2(tablero, n, xVacio, yVacio, 0, 0);
-		} else {
-			throw new IllegalArgumentException("Error en los parámetros.");
-		}
 	}
 
 }
--- src/p3/tromino/TrominoTiempos.java
+++ src/p3/tromino/TrominoTiempos.java
@@ -4,7 +4,7 @@ public class TrominoTiempos {
 
 	public static void main(String[] args) {
 		Tromino tromino = new Tromino();
-		tromino.crearTablero_2(8, 2, 1);
-//		tromino.imprimirTablero();
+
+		tromino.crearTablero(8, 4, 6);
 	}
 }
