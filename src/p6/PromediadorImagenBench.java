bff827978dfa8b47a14bbf8cddca46cdd3e89c9f
uo277303 <uo277303@EII-L05-12.aulasuo.uniovi.es>
2022-04-01 15:48:15 +0200

    Errores arreglados

--- src/p6/PromediadorImagenBench.java
+++ src/p6/PromediadorImagenBench.java
@@ -2,15 +2,13 @@ package p6;
 
 import java.nio.file.Paths;
 
-import s6.ImageAverager;
-
 public class PromediadorImagenBench {
 
 	// Ajustes del banco de pruebas
-	private static String REAL_IMG = Paths.get("").toAbsolutePath().toString() + "files\\einstein_1_256.png";
-	private static String BAD_IMG = Paths.get("").toAbsolutePath().toString() + "files\\einstein_1_256.png";
-	private static String OUT_DIR_G = Paths.get("").toAbsolutePath().toString() + "/algprofesor/s6_es/out_g/";
-	private static String OUT_DIR_B = Paths.get("").toAbsolutePath().toString() + "/algprofesor/s6_es/out_bt";
+	private static String REAL_IMG = Paths.get("").toAbsolutePath().toString() + "/p6/einstein_1_256.png";
+	private static String BAD_IMG = Paths.get("").toAbsolutePath().toString() + "/p6/einstein_1_256.png";
+	private static String OUT_DIR_G = Paths.get("").toAbsolutePath().toString() + "/p6/out_g/";
+	private static String OUT_DIR_B = Paths.get("").toAbsolutePath().toString() + "/p6/out_bt";
 	private static int N_IMGS = 13;
 	private static double PORCENTAJE_BAD = 25; // %
 	private static double S_NOISE = 5.0; // Nivel de ruido - desvición estándar de una distrubución Gaussiana
@@ -18,12 +16,12 @@ public class PromediadorImagenBench {
 	public static void main(String[] args) {
 
 		int n_real, n_bad;
-		ImageAverager img_avger;
+		PromediadorImagen img_avger;
 
 		// Generación y testeo de un conjunto de imágenes
 		n_bad = (int) ((PORCENTAJE_BAD / 100.) * N_IMGS);
 		n_real = N_IMGS - n_bad;
-		img_avger = new ImageAverager(REAL_IMG, BAD_IMG, n_real, n_bad, S_NOISE);
+		img_avger = new PromediadorImagen(REAL_IMG, BAD_IMG, n_real, n_bad, S_NOISE);
 
 		System.out.print("TESTING VORAZ:\n");
 		img_avger.splitSubsetsGreedy(N_IMGS);
