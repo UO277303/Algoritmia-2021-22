7353748169c23239eb49ed4b993006d68f281034
Héctor Lavandeira <79642990+UO277303@users.noreply.github.com>
2022-04-27 20:31:23 +0200

    trominó paralelo (no funciona)

--- src/p3/tromino/TrominoParalelo.java
+++ src/p3/tromino/TrominoParalelo.java
@@ -1,5 +1,171 @@
 package p3.tromino;
 
-public class TrominoParalelo {
+import java.util.concurrent.RecursiveAction;
+
+public class TrominoParalelo extends RecursiveAction {
+
+	private static final long serialVersionUID = 1L;
+
+	int[][] tablero;
+	int count = 0;
+
+	private int x;
+	private int y;
+	private int n;
+	private int orig_x;
+	private int orig_y;
+
+	public TrominoParalelo(int x, int y, int n, int orig_x, int orig_y) {
+		if (n > 0 && potenciaDe2(n) && x < n && y < n) {
+			this.x = x;
+			this.y = y;
+			this.n = n;
+			this.orig_x = orig_x;
+			this.orig_y = orig_y;
+			this.tablero = new int[n][n];
+			this.tablero[x][y] = -1;
+		} else {
+			throw new IllegalArgumentException("Error en los parámetros.");
+		}
+	}
+
+	private boolean potenciaDe2(int x) {
+		return Math.log(x) / Math.log(2) % 1 == 0;
+	}
+
+	@Override
+	protected void compute() {
+		int cuadrante = buscarCuadrante(orig_x + n / 2 - 1, orig_y + n / 2 - 1, x, y);
+
+		// Centro del subtablero
+		int[] c = { orig_x + n / 2 - 1, orig_y + n / 2 - 1 };
+
+		count++;
+
+		if (n > 2) {
+			if (cuadrante == 1) {
+				tablero[c[0]][c[1] + 1] = count;
+				tablero[c[0] + 1][c[1]] = count;
+				tablero[c[0] + 1][c[1] + 1] = count;
+
+				TrominoParalelo rec1 = new TrominoParalelo(x, y, n / 2, orig_x, orig_y);
+				TrominoParalelo rec2 = new TrominoParalelo(c[0], c[1] + 1, n / 2, orig_x, orig_y + n / 2);
+				TrominoParalelo rec3 = new TrominoParalelo(c[0] + 1, c[1], n / 2, orig_x + n / 2, orig_y);
+				TrominoParalelo rec4 = new TrominoParalelo(c[0] + 1, c[1] + 1, n / 2, orig_x + n / 2, orig_y + n / 2);
+
+				invokeAll(rec1, rec2, rec3, rec4);
+
+				juntarResultados(n, rec1.tablero, rec2.tablero, rec3.tablero, rec4.tablero);
+
+			} else if (cuadrante == 2) {
+				tablero[c[0]][c[1]] = count;
+				tablero[c[0] + 1][c[1]] = count;
+				tablero[c[0] + 1][c[1] + 1] = count;
+
+				TrominoParalelo rec1 = new TrominoParalelo(c[0], c[1], n / 2, orig_x, orig_y);
+				TrominoParalelo rec2 = new TrominoParalelo(c[0] + 1, c[1], n / 2, orig_x, orig_y + n / 2);
+				TrominoParalelo rec3 = new TrominoParalelo(x, y, n / 2, orig_x + n / 2, orig_y);
+				TrominoParalelo rec4 = new TrominoParalelo(c[0] + 1, c[1] + 1, n / 2, orig_x + n / 2, orig_y + n / 2);
+
+				invokeAll(rec1, rec2, rec3, rec4);
+
+				juntarResultados(n, rec1.tablero, rec2.tablero, rec3.tablero, rec4.tablero);
+
+			} else if (cuadrante == 3) {
+				tablero[c[0]][c[1]] = count;
+				tablero[c[0]][c[1] + 1] = count;
+				tablero[c[0] + 1][c[1] + 1] = count;
+
+				TrominoParalelo rec1 = new TrominoParalelo(c[0], c[1], n / 2, orig_x, orig_y);
+				TrominoParalelo rec2 = new TrominoParalelo(x, y, n / 2, orig_x, orig_y + n / 2);
+				TrominoParalelo rec3 = new TrominoParalelo(c[0], c[1] + 1, n / 2, orig_x + n / 2, orig_y);
+				TrominoParalelo rec4 = new TrominoParalelo(c[0] + 1, c[1] + 1, n / 2, orig_x + n / 2, orig_y + n / 2);
+
+				invokeAll(rec1, rec2, rec3, rec4);
+
+				juntarResultados(n, rec1.tablero, rec2.tablero, rec3.tablero, rec4.tablero);
+
+			} else {
+				tablero[c[0]][c[1]] = count;
+				tablero[c[0] + 1][c[1]] = count;
+				tablero[c[0]][c[1] + 1] = count;
+
+				TrominoParalelo rec1 = new TrominoParalelo(c[0], c[1], n / 2, orig_x, orig_y);
+				TrominoParalelo rec2 = new TrominoParalelo(c[0], c[1] + 1, n / 2, orig_x, orig_y + n / 2);
+				TrominoParalelo rec3 = new TrominoParalelo(c[0] + 1, c[1], n / 2, orig_x + n / 2, orig_y);
+				TrominoParalelo rec4 = new TrominoParalelo(x, y, n / 2, orig_x + n / 2, orig_y + n / 2);
+
+				invokeAll(rec1, rec2, rec3, rec4);
+
+				juntarResultados(n, rec1.tablero, rec2.tablero, rec3.tablero, rec4.tablero);
+
+			}
+		} else { // Caso básico
+			if (tablero[orig_x][orig_y] != 0) {
+
+				tablero[orig_x + 1][orig_y] = count;
+				tablero[orig_x][orig_y + 1] = count;
+				tablero[orig_x + 1][orig_y + 1] = count;
+
+			} else if (tablero[orig_x + 1][orig_y] != 0) {
+
+				tablero[orig_x][orig_y] = count;
+				tablero[orig_x][orig_y + 1] = count;
+				tablero[orig_x + 1][orig_y + 1] = count;
+
+			} else if (tablero[orig_x][orig_y + 1] != 0) {
+
+				tablero[orig_x][orig_y] = count;
+				tablero[orig_x + 1][orig_y] = count;
+				tablero[orig_x + 1][orig_y + 1] = count;
+			} else {
+
+				tablero[orig_x][orig_y] = count;
+				tablero[orig_x + 1][orig_y] = count;
+				tablero[orig_x][orig_y + 1] = count;
+			}
+		}
+	}
+
+	private int buscarCuadrante(int x, int y, int posX, int posY) {
+		if (posX <= x && posY <= y) {
+			return 1;
+		} else if (posX <= x && posY > y) {
+			return 2;
+		} else if (posX > x && posY <= y) {
+			return 3;
+		}
+		return 4;
+	}
+
+	private int[][] juntarResultados(int tamaño, int[][] r1, int[][] r2, int[][] r3, int[][] r4) {
+		int[][] resultado = new int[tamaño][tamaño];
+
+		for (int i = 0; i < tamaño / 2; i++) {
+			for (int j = 0; j < tamaño / 2; j++) {
+				resultado[i][j] = r1[i][j];
+			}
+		}
+
+		for (int i = tamaño / 2; i < tamaño; i++) {
+			for (int j = 0; j < tamaño / 2; j++) {
+				resultado[i][j] = r2[i - tamaño / 2][j];
+			}
+		}
+
+		for (int i = 0; i < tamaño / 2; i++) {
+			for (int j = tamaño / 2; j < tamaño; j++) {
+				resultado[i][j] = r3[i][j - tamaño / 2];
+			}
+		}
+
+		for (int i = tamaño / 2; i < tamaño; i++) {
+			for (int j = tamaño / 2; j < tamaño; j++) {
+				resultado[i][j] = r4[i - tamaño / 2][j - tamaño / 2];
+			}
+		}
+
+		return resultado;
+	}
 
 }
--- src/p3/tromino/TrominoTiempos.java
+++ src/p3/tromino/TrominoTiempos.java
@@ -1,18 +1,40 @@
 package p3.tromino;
 
+import java.util.concurrent.ForkJoinPool;
+
 public class TrominoTiempos {
 
 	public static void main(String[] args) {
-		int rep = 1;
+		int rep = 10000;
 
 		Tromino tromino = new Tromino();
 
+		System.out.println("Algoritmo secuencial:");
 		System.out.println("n\tt\trep");
-		for (int n = 2; n < 1_000_000; n *= 2) {
+		for (int n = 2; n < 100; n *= 2) {
 			long t1 = System.currentTimeMillis();
 
 			for (int i = 0; i < rep; i++) {
 				tromino.crearTablero(n, 0, 0);
+			}
+
+			long t2 = System.currentTimeMillis() - t1;
+
+			System.out.println(n + "\t" + t2 + "\t" + rep);
+		}
+
+		rep = 1000000;
+
+		System.out.println("\nAlgoritmo paralelo:");
+		System.out.println("n\tt\trep");
+		for (int n = 2; n < 1_000; n *= 2) {
+			TrominoParalelo tromParalelo = new TrominoParalelo(0, 0, n, 0, 0);
+			ForkJoinPool pool = new ForkJoinPool();
+
+			long t1 = System.currentTimeMillis();
+
+			for (int i = 0; i < rep; i++) {
+				pool.invoke(tromParalelo);
 			}
 
 			long t2 = System.currentTimeMillis() - t1;
