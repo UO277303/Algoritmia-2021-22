86575879c1b868dd6a066d6ffd7bb6efa72aac53
uo277303 <uo277303@EII-L05-12.aulasuo.uniovi.es>
2022-05-06 16:03:56 +0200

    defensa examen

---
+++ src/p6_examen/PromediadorImagen.java
@@ -0,0 +1,572 @@
+package p6_examen;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PromediadorImagen {
+
+	private Imagen real_img, bad_img; // para almacenar las imagenes con patron bueno y malo (negativo del malo)
+	private Imagen avg_img, half1_img, half2_img; // para almacenar los promedios del subconjunto 1 y 2
+	private Imagen[] dataset; // almacena el conjunto de de imagenes generadas (buenas y malas)
+	private int[] sol; // array que determina donde poner las imágenes 0->no asignada, 1->primer
+						// subconjunto, 2->segundo subconjunto
+	private int[] bestSol; // mejor solución
+	private int width, height; // ancho y alto de las imágenes
+	// backtracking variables
+	private int counter; // contador de nodos en el arbol implícito
+	private double max_zncc; // donde almacenar el ZNCC final
+
+	// variables para contar el número de 1 y 2 que se guardan en el vector sol
+	private int numUnos;
+	private int numDoses;
+
+	/**
+	 * Constructor
+	 * 
+	 * @real_path ruta del modelo de imagen "buena" (patrón a encontrar) en disco
+	 * @bad_path ruta del modelo de imagen "mala"
+	 * @n_real numero de imagenes buenas (>= 1)
+	 * @n_bad numero de imagenes "malas" (tiene que ser menor que las buenas)
+	 * @s_noise standard deviation for noise
+	 */
+	public PromediadorImagen(String real_path, String bad_path, int n_real, int n_bad, double s_noise) {
+
+		assert (n_real >= 1) && (n_bad < n_real);
+
+		// Cargando los patrones de referencia (buena y mala)
+		this.real_img = new Imagen(real_path);
+		this.bad_img = new Imagen(bad_path);
+		this.width = this.real_img.getWidth();
+		this.height = this.real_img.getHeight();
+
+		// Se crean con conjunto de imagenes con un array ordenado aleatoriamente para
+		// posicionar
+		// las imagenes buenas y malas aleatoriamente
+		int total_imgs = n_real + n_bad; // numero total de imágenes
+		this.dataset = new Imagen[total_imgs];
+		this.sol = new int[total_imgs]; // dónde se almacena la solución actual (combinación de asignaciones): 0->no
+										// asignada, 1->primer subconjunto, 2->segundo subconjunto
+		this.bestSol = new int[total_imgs]; // dónde se almacena la mejor solución
+		int[] rand_index = this.randomIndexes(total_imgs); // array con las posiciones aleatorias
+		Imagen hold_img; // imagen temporal
+		int region = 0; // 0-arriba, 1-bajo, 2-izquierda, 3-derecha
+		for (int i = 0; i < n_real; i++) { // imágenes buenas
+			hold_img = new Imagen(this.width, this.height); // creación de la imagen
+			hold_img.addSignal(this.real_img); // añadir los valores de los píxeles
+			hold_img.suppressRegion(region); // suprimir una region
+			hold_img.addNoise(s_noise); // añadir ruido
+			this.dataset[rand_index[i]] = hold_img; // incluir la imagne en una posción aleatorio de dataset
+			if (region == 3)
+				region = 0;
+			else
+				region++;
+		}
+		region = 0;
+		for (int i = n_real; i < n_real + n_bad; i++) { // bucle para las imágenes malas
+			hold_img = new Imagen(this.width, this.height);
+			hold_img.addSignal(this.bad_img);
+			hold_img.invertSignal();
+			hold_img.suppressRegion(region);
+			hold_img.addNoise(s_noise);
+			this.dataset[rand_index[i]] = hold_img;
+			if (region == 3)
+				region = 0;
+			else
+				region++;
+		}
+	}
+
+	/**
+	 * Gener una array con valores de posiciones aleatorios
+	 * 
+	 * @param n longitud del array
+	 * @return
+	 */
+	public int[] randomIndexes(int n) {
+		List<Integer> list = new ArrayList<>();
+		for (int i = 0; i < n; i++)
+			list.add(i);
+		Collections.shuffle(list);
+		int[] array = new int[n];
+		for (int i = 0; i < n; i++)
+			array[i] = list.get(i);
+		return array;
+	}
+
+	/**
+	 * Almacena las imágenes generadas según la mejor solución encontrada
+	 * 
+	 * @out_dir directorio donde se almacenan las imágenes
+	 */
+	public void saveResults(String out_dir) {
+		this.avg_img.save(out_dir + "/img_avg.png");
+		this.half1_img.save(out_dir + "/img_half1_avg.png");
+		this.half2_img.save(out_dir + "/img_half2_avg.png");
+		for (int i = 0; i < this.dataset.length; i++) {
+			this.dataset[i].save(out_dir + "/img_" + i + "_klass_" + this.bestSol[i] + ".png");
+		}
+	}
+
+	/**
+	 * @return devuelve el número de pasos requeridos por el algoritmo
+	 */
+	public int getCounter() {
+		return counter;
+	}
+
+	/**
+	 * Calcula el ZNCC entre los promedios de los dos subconjuntos de imágenes
+	 * 
+	 * @return el valor de ZNCC
+	 */
+	public double zncc() {
+		return this.half1_img.zncc(this.half2_img);
+	}
+
+	/**
+	 * Greedy algorithm: random instances for each half, the best one is the final
+	 * solution
+	 * 
+	 * @n_tries numero de intentos aleatorios
+	 */
+	public void splitSubsetsGreedy(int n_tries) {
+		int count = n_tries;
+		double zncc = -1;
+
+		this.half1_img = new Imagen(width, height); // Inicializo las imágenes resultado
+		this.half2_img = new Imagen(width, height);
+		this.avg_img = new Imagen(width, height);
+
+		while (count > 0) {
+
+			Imagen img1 = new Imagen(width, height); // Creo dos imágenes auxiliares
+			Imagen img2 = new Imagen(width, height);
+
+			int[] v = new int[dataset.length];
+			for (int i = 0; i < v.length; i++) { // Relleno aleatoriamente el vector solución
+				v[i] = (int) (Math.random() * 3);
+			}
+
+			this.sol = v;
+
+			for (int i = 0; i < v.length; i++) { // Fusiono las imágenes de cada grupo
+				if (v[i] == 1) {
+					img1.addSignal(dataset[i]);
+				} else if (v[i] == 2) {
+					img2.addSignal(dataset[i]);
+				}
+			}
+
+			double new_zncc = img1.zncc(img2); // Calculo el zncc de las imágenes auxiliares
+
+			if (new_zncc > zncc) { // Si es mayor:
+				zncc = new_zncc;
+				this.bestSol = sol.clone();
+				this.half1_img = img1.copy(); // Copio las imágenes a las resultado
+				this.half2_img = img2.copy();
+				this.avg_img.addSignal(this.half1_img); // Las fusiono en la imágen final
+				this.avg_img.addSignal(this.half2_img);
+			}
+
+			count--;
+			this.counter++;
+		}
+
+//		printSol('v');
+	}
+
+	/**
+	 * Algoritmo backtracking con condición balanceo
+	 * 
+	 * @max_unbalancing: (condición de poda) determina la diferencia máxima en el
+	 *                   número de imágenes entre los dos subconjuntos
+	 */
+	public void splitSubsetsBacktracking(int max_unbalancing) {
+
+		this.half1_img = new Imagen(width, height);
+		this.half2_img = new Imagen(width, height);
+		this.avg_img = new Imagen(width, height);
+		this.max_zncc = -1;
+		this.counter = 0;
+		this.numUnos = 0;
+		this.numDoses = 0;
+
+		backtrackingBalanceoRec(0, max_unbalancing);
+
+//		printSol('n');
+	}
+
+	private void backtrackingBalanceoRec(int level, int max_unbalancing) {
+
+		if (level == sol.length) { // Caso base
+
+			if (Math.abs(numUnos - numDoses) <= max_unbalancing) { // Compruebo que haya +- el mismo nº de 1 y 2
+				Imagen img1 = new Imagen(width, height);
+				Imagen img2 = new Imagen(width, height);
+
+				for (int i = 0; i < sol.length; i++) {
+					if (sol[i] == 1) {
+						img1.addSignal(dataset[i]);
+					} else if (sol[i] == 2) {
+						img2.addSignal(dataset[i]);
+					}
+				}
+
+				double new_zncc = img1.zncc(img2);
+
+				if (new_zncc > max_zncc) {
+					max_zncc = new_zncc;
+					this.bestSol = sol.clone();
+					this.half1_img = img1.copy();
+					this.half2_img = img2.copy();
+					this.avg_img.addSignal(this.half1_img);
+					this.avg_img.addSignal(this.half2_img);
+				}
+			}
+
+		} else {
+			if (Math.abs(numUnos - numDoses) <= max_unbalancing) {
+				for (int i = 0; i < 3; i++) {
+					this.counter++;
+					if (i == 1) { // Aumento el contados de unos o doses según corresponda
+						numUnos++;
+					} else if (i == 2) {
+						numDoses++;
+					}
+					sol[level] = i;
+					backtrackingBalanceoRec(level + 1, max_unbalancing);
+					if (i == 1) { // Lo pongo al valor anterior
+						numUnos--;
+					} else if (i == 2) {
+						numDoses--;
+					}
+				}
+			}
+		}
+
+	}
+
+	/**
+	 * Algoritmo backtracking sin condición de balanceo.
+	 */
+	public void splitSubsetsBacktracking() {
+
+		this.half1_img = new Imagen(width, height);
+		this.half2_img = new Imagen(width, height);
+		this.avg_img = new Imagen(width, height);
+		this.max_zncc = -1;
+		this.counter = 0;
+
+		backtrackingRec(0);
+
+//		printSol('b');
+	}
+
+	private void backtrackingRec(int level) {
+
+		if (level == sol.length) { // Caso base
+
+			Imagen img1 = new Imagen(width, height);
+			Imagen img2 = new Imagen(width, height);
+
+			for (int i = 0; i < sol.length; i++) {
+				if (sol[i] == 1) {
+					img1.addSignal(dataset[i]);
+				} else if (sol[i] == 2) {
+					img2.addSignal(dataset[i]);
+				}
+			}
+
+			double new_zncc = img1.zncc(img2);
+
+			if (new_zncc > max_zncc) {
+				max_zncc = new_zncc;
+				this.bestSol = sol.clone();
+				this.half1_img = img1.copy();
+				this.half2_img = img2.copy();
+				this.avg_img.addSignal(this.half1_img);
+				this.avg_img.addSignal(this.half2_img);
+			}
+		} else {
+			for (int i = 0; i < 3; i++) {
+				this.counter++;
+				sol[level] = i;
+				backtrackingRec(level + 1);
+			}
+		}
+
+	}
+
+//	private void printSol(char tipo) {
+//		if (tipo == 'v') {
+//			System.out.print("Voraz: ");
+//		} else if (tipo == 'b') {
+//			System.out.print("Backt: ");
+//		} else {
+//			System.out.print("Balan: ");
+//		}
+//		for (int i = 0; i < this.bestSol.length; i++) {
+//			if (i < this.bestSol.length - 1) {
+//				System.out.print(bestSol[i] + " - ");
+//			} else {
+//				System.out.print(bestSol[i] + "\n");
+//			}
+//		}
+//	}
+
+//	private void printSol() {
+//		for (int i = 0; i < this.bestSol.length; i++) {
+//			if (i < this.bestSol.length - 1) {
+//				System.out.print(bestSol[i] + " - ");
+//			} else {
+//				System.out.print(bestSol[i] + "\n");
+//			}
+//		}
+//	}
+
+	// CÓDIGO DEFENSA
+
+	public void vorazExamenSinBalanceo(int n_tries) {
+		int count = n_tries;
+		double zncc = -1;
+
+		this.half1_img = new Imagen(width, height);
+		this.half2_img = new Imagen(width, height);
+		this.avg_img = new Imagen(width, height);
+
+		this.counter = 0;
+
+		while (count > 0) {
+
+			Imagen img1 = new Imagen(width, height);
+			Imagen img2 = new Imagen(width, height);
+
+			int[] v = new int[dataset.length];
+			for (int i = 0; i < v.length; i++) {
+				v[i] = (int) (Math.random() * 4);
+			}
+
+			this.sol = v;
+
+			for (int i = 0; i < v.length; i++) {
+				if (v[i] == 1) {
+					img1.addSignal(dataset[i]);
+				} else if (v[i] == 2) {
+					img2.addSignal(dataset[i]);
+				} else if (v[i] == 3) {
+					img1.addSignal(dataset[i]);
+					img2.addSignal(dataset[i]);
+				}
+			}
+
+			double new_zncc = img1.zncc(img2);
+
+			if (new_zncc > zncc) {
+				zncc = new_zncc;
+				this.bestSol = sol.clone();
+				this.half1_img = img1.copy();
+				this.half2_img = img2.copy();
+				this.avg_img.addSignal(this.half1_img);
+				this.avg_img.addSignal(this.half2_img);
+			}
+
+			count--;
+			this.counter++;
+		}
+
+//		printSol();
+	}
+
+	public void vorazExamenConBalanceo(int n_tries, int max_unbalancing) {
+		int count = n_tries;
+		double zncc = -1;
+
+		this.half1_img = new Imagen(width, height);
+		this.half2_img = new Imagen(width, height);
+		this.avg_img = new Imagen(width, height);
+
+		this.numUnos = 0;
+		this.numDoses = 0;
+		this.counter = 0;
+
+		while (count > 0) {
+
+			Imagen img1 = new Imagen(width, height);
+			Imagen img2 = new Imagen(width, height);
+
+			int[] v = new int[dataset.length];
+			for (int i = 0; i < v.length; i++) {
+				int random = (int) (Math.random() * 4);
+				if (random == 1) {
+					numUnos++;
+				} else if (random == 2) {
+					numDoses++;
+				} else if (random == 3) {
+					numUnos++;
+					numDoses++;
+				}
+				if (Math.abs(numUnos - numDoses) <= max_unbalancing) {
+					v[i] = random;
+				}
+			}
+
+			this.sol = v;
+
+			for (int i = 0; i < v.length; i++) {
+				if (v[i] == 1) {
+					img1.addSignal(dataset[i]);
+				} else if (v[i] == 2) {
+					img2.addSignal(dataset[i]);
+				} else if (v[i] == 3) {
+					img1.addSignal(dataset[i]);
+					img2.addSignal(dataset[i]);
+				}
+			}
+
+			double new_zncc = img1.zncc(img2);
+
+			if (new_zncc > zncc) {
+				zncc = new_zncc;
+				this.bestSol = sol.clone();
+				this.half1_img = img1.copy();
+				this.half2_img = img2.copy();
+				this.avg_img.addSignal(this.half1_img);
+				this.avg_img.addSignal(this.half2_img);
+			}
+
+			count--;
+			this.counter++;
+
+		}
+
+//		printSol();
+	}
+
+	public void backtrackingExamenSinBalanceo() {
+
+		this.half1_img = new Imagen(width, height);
+		this.half2_img = new Imagen(width, height);
+		this.avg_img = new Imagen(width, height);
+
+		this.max_zncc = -1;
+		this.counter = 0;
+
+		backtrackingExamenRec(0);
+
+//		printSol();
+	}
+
+	private void backtrackingExamenRec(int level) {
+
+		if (level == sol.length) { // Caso base
+
+			Imagen img1 = new Imagen(width, height);
+			Imagen img2 = new Imagen(width, height);
+
+			for (int i = 0; i < sol.length; i++) {
+				if (sol[i] == 1) {
+					img1.addSignal(dataset[i]);
+				} else if (sol[i] == 2) {
+					img2.addSignal(dataset[i]);
+				} else if (sol[i] == 3) {
+					img1.addSignal(dataset[i]);
+					img2.addSignal(dataset[i]);
+				}
+			}
+
+			double new_zncc = img1.zncc(img2);
+
+			if (new_zncc > max_zncc) {
+				max_zncc = new_zncc;
+				this.bestSol = sol.clone();
+				this.half1_img = img1.copy();
+				this.half2_img = img2.copy();
+				this.avg_img.addSignal(this.half1_img);
+				this.avg_img.addSignal(this.half2_img);
+			}
+		} else {
+			for (int i = 0; i < 4; i++) {
+				this.counter++;
+				sol[level] = i;
+				backtrackingExamenRec(level + 1);
+			}
+		}
+
+	}
+
+	public void backtrackingExamenConBalanceo(int max_unbalancing) {
+
+		this.half1_img = new Imagen(width, height);
+		this.half2_img = new Imagen(width, height);
+		this.avg_img = new Imagen(width, height);
+		this.max_zncc = -1;
+		this.counter = 0;
+		this.numUnos = 0;
+		this.numDoses = 0;
+
+		backtrackingExamenBalanceoRec(0, max_unbalancing);
+
+//		printSol();
+	}
+
+	private void backtrackingExamenBalanceoRec(int level, int max_unbalancing) {
+
+		if (level == sol.length) {
+
+			if (Math.abs(numUnos - numDoses) <= max_unbalancing) {
+				Imagen img1 = new Imagen(width, height);
+				Imagen img2 = new Imagen(width, height);
+
+				for (int i = 0; i < sol.length; i++) {
+					if (sol[i] == 1) {
+						img1.addSignal(dataset[i]);
+					} else if (sol[i] == 2) {
+						img2.addSignal(dataset[i]);
+					} else if (sol[i] == 3) {
+						img1.addSignal(dataset[i]);
+						img2.addSignal(dataset[i]);
+					}
+				}
+
+				double new_zncc = img1.zncc(img2);
+
+				if (new_zncc > max_zncc) {
+					max_zncc = new_zncc;
+					this.bestSol = sol.clone();
+					this.half1_img = img1.copy();
+					this.half2_img = img2.copy();
+					this.avg_img.addSignal(this.half1_img);
+					this.avg_img.addSignal(this.half2_img);
+				}
+			}
+
+		} else {
+			if (Math.abs(numUnos - numDoses) <= max_unbalancing) {
+				for (int i = 0; i < 4; i++) {
+					this.counter++;
+					if (i == 1) {
+						numUnos++;
+					} else if (i == 2) {
+						numDoses++;
+					} else if (i == 3) {
+						numUnos++;
+						numDoses++;
+					}
+					sol[level] = i;
+					backtrackingExamenBalanceoRec(level + 1, max_unbalancing);
+					if (i == 1) {
+						numUnos--;
+					} else if (i == 2) {
+						numDoses--;
+					} else if (i == 3) {
+						numUnos--;
+						numDoses--;
+					}
+				}
+			}
+		}
+
+	}
+
+}
---
+++ src/p6_examen/PromediadorImagenBench.java
@@ -0,0 +1,79 @@
+package p6_examen;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.nio.file.Paths;
+
+public class PromediadorImagenBench {
+
+	// Ajustes del banco de pruebas
+	private static String REAL_IMG = Paths.get("").toAbsolutePath().toString() + "/src/p6_examen/einstein_1_256.png";
+	private static String BAD_IMG = Paths.get("").toAbsolutePath().toString() + "/src/p6_examen/einstein_1_256.png";
+	private static String OUT_DIR_G = Paths.get("").toAbsolutePath().toString() + "/src/p6_examen/out_g/";
+	private static String OUT_DIR_B = Paths.get("").toAbsolutePath().toString() + "/src/p6_examen/out_bt";
+	private static int N_IMGS = 6;
+	private static double PORCENTAJE_BAD = 25; // %
+	private static double S_NOISE = 5.0; // Nivel de ruido - desvición estándar de una distrubución Gaussiana
+
+	public static void main(String[] args) {
+
+		int n_real, n_bad;
+		PromediadorImagen img_avger;
+
+		// Generación y testeo de un conjunto de imágenes
+		n_bad = (int) ((PORCENTAJE_BAD / 100.) * N_IMGS);
+		n_real = N_IMGS - n_bad;
+		img_avger = new PromediadorImagen(REAL_IMG, BAD_IMG, n_real, n_bad, S_NOISE);
+
+//		System.out.print("TESTING EXAMEN VORAZ SIN BALANCEO:\n");
+//		img_avger.vorazExamenSinBalanceo(N_IMGS);
+//		System.out.printf("  -ZNCC: %f\n", img_avger.zncc());
+//		System.out.printf("  -Contador: %d\n", img_avger.getCounter());
+//		img_avger.saveResults(OUT_DIR_G);
+//
+//		System.out.print("TESTING EXAMEN VORAZ CON BALANCEO:\n");
+//		img_avger.vorazExamenConBalanceo(N_IMGS, 1);
+//		System.out.printf("  -ZNCC: %f\n", img_avger.zncc());
+//		System.out.printf("  -Contador: %d\n", img_avger.getCounter());
+//		img_avger.saveResults(OUT_DIR_G);
+//
+//		System.out.print("TESTING EXAMEN BACKTRACKING SIN BALANCEO:\n");
+//		img_avger.backtrackingExamenSinBalanceo();
+//		System.out.printf("  -ZNCC: %f\n", img_avger.zncc());
+//		System.out.printf("  -Contador: %d\n", img_avger.getCounter());
+//		img_avger.saveResults(OUT_DIR_B);
+//
+//		System.out.print("TESTING EXAMEN BACKTRACKING CON BALANCEO:\n");
+//		img_avger.backtrackingExamenConBalanceo(1);
+//		System.out.printf("  -ZNCC: %f\n", img_avger.zncc());
+//		System.out.printf("  -Contador: %d\n", img_avger.getCounter());
+//		img_avger.saveResults(OUT_DIR_B);
+
+		// Medidas
+		System.out.println("n\tt_bt_p6\tt_bt_ex");
+		for (int i = 2; i < 20; i++) {
+			n_bad = (int) ((PORCENTAJE_BAD / 100.) * i);
+			n_real = i - n_bad;
+			img_avger = new PromediadorImagen(REAL_IMG, BAD_IMG, n_real, n_bad, S_NOISE);
+
+			img_avger.splitSubsetsGreedy(i);
+
+			long t1 = System.currentTimeMillis();
+			img_avger.splitSubsetsBacktracking();
+			long t_bt_p6 = System.currentTimeMillis() - t1;
+
+			t1 = System.currentTimeMillis();
+			img_avger.backtrackingExamenSinBalanceo();
+			long t_bt_ex = System.currentTimeMillis() - t1;
+
+			System.out.println(i + "\t" + t_bt_p6 + "\t" + t_bt_ex);
+		}
+	}
+
+	public static double round(double valor, int nDec) {
+		BigDecimal bd = BigDecimal.valueOf(valor);
+		bd = bd.setScale(nDec, RoundingMode.HALF_UP);
+		return bd.doubleValue();
+	}
+
+}
